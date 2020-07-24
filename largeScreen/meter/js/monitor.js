1427d871c909885ac752254d5e59463969f3ed1f
xinshaoguang <58464732+xinshaoguang@users.noreply.github.com>
2020-07-24 17:48:04 +0800

    样式完成80%

--- largeScreen/meter/js/monitor.js
+++ largeScreen/meter/js/monitor.js
@@ -1,104 +1,104 @@
-// 负载均衡图表
-var loadBalance = echarts.init(document.getElementById('load-balance'));
-var loadBalanceOption = {
+// // 负载均衡图表
+// var loadBalance = echarts.init(document.getElementById('load-balance'));
+// var loadBalanceOption = {
 
-    legend: {
-        data: ['图例1', '图例2', '图例3', '图例4'],
-        bottom: 20,
-        textStyle: {
-            color: '#ffffff',
-        }
-    },
-    graphic: {
-        elements: [
-            {
-                type: 'text',
-                left: 'center', // 相对父元素居中
-                top: '150',  // 相对父元素上下的位置
-                style: {
-                    fill: '#FFFF00',
-                    text: [''],
-                    font: '34px Arial Normal',
-                }
-            }]
-    },
-    series: [
-        {
-            type: 'pie',
-            center: ['50%', '45%'],
-            tooltip: {
-                trigger: 'item',
-                formatter: "{a} <br/>{b}: {c} ({d}%)"
-            },
-            radius: ['82', '117'],
-            label: {
-                verticalAlign: 'bottom',
-                fontSize: 16,
-                color: '#00FFFF',
-                verticalAlign: 'top',
+//     legend: {
+//         data: ['图例1', '图例2', '图例3', '图例4'],
+//         bottom: 20,
+//         textStyle: {
+//             color: '#ffffff',
+//         }
+//     },
+//     graphic: {
+//         elements: [
+//             {
+//                 type: 'text',
+//                 left: 'center', // 相对父元素居中
+//                 top: '150',  // 相对父元素上下的位置
+//                 style: {
+//                     fill: '#FFFF00',
+//                     text: [''],
+//                     font: '34px Arial Normal',
+//                 }
+//             }]
+//     },
+//     series: [
+//         {
+//             type: 'pie',
+//             center: ['50%', '45%'],
+//             tooltip: {
+//                 trigger: 'item',
+//                 formatter: "{a} <br/>{b}: {c} ({d}%)"
+//             },
+//             radius: ['82', '117'],
+//             label: {
+//                 verticalAlign: 'bottom',
+//                 fontSize: 16,
+//                 color: '#00FFFF',
+//                 verticalAlign: 'top',
 
-                // normal: {
-                //     show: true,
-                //     // position: 'center',
-                //     formatter: '{b}',
-                //     verticalAlign: 'middle',
-                //     verticalAlign: 'middle',
-                //     textStyle: {
-                //         color: '#00FFFF',
-                //         // fontSize: '16',
-                //         backgroundColor: '#f00',
-                //     },
-                // },
-                // emphasis: {
-                //     show: true,
-                //     textStyle: {
-                //         fontSize: '16',
-                //         fontWeight: 'bold',
-                //         color: '#00FFFF',
-                //     },
+//                 // normal: {
+//                 //     show: true,
+//                 //     // position: 'center',
+//                 //     formatter: '{b}',
+//                 //     verticalAlign: 'middle',
+//                 //     verticalAlign: 'middle',
+//                 //     textStyle: {
+//                 //         color: '#00FFFF',
+//                 //         // fontSize: '16',
+//                 //         backgroundColor: '#f00',
+//                 //     },
+//                 // },
+//                 // emphasis: {
+//                 //     show: true,
+//                 //     textStyle: {
+//                 //         fontSize: '16',
+//                 //         fontWeight: 'bold',
+//                 //         color: '#00FFFF',
+//                 //     },
 
-                // },
-            },
-            labelLine: {
-                lineStyle: {
-                    color: '#00FFFF',
-                },
-            },
-            // hoverAnimation: false,
-            itemStyle: {
-                // borderColor: '#00FFFF'
-                // shadowColor: '#00FFFF',
-                // shadowBlur: 3
-            },
-            data: [
-                {
-                    value: 335, name: '图例1',
-                },
-                {value: 310, name: '图例2'},
-                {value: 234, name: '图例3'},
-                {value: 135, name: '图例4'},
-            ]
-        },
-    ]
-}
-loadBalance.on('mouseover', function (params) {
-    loadBalanceOption.graphic.elements[0].style.text = [params.percent + '%']
-    loadBalance.setOption(loadBalanceOption);
-});
-loadBalance.on('mouseout', function (p) {
-    loadBalanceOption.graphic.elements[0].style.text = ['']
-    loadBalance.setOption(loadBalanceOption);
-})
-loadBalance.setOption(loadBalanceOption);
+//                 // },
+//             },
+//             labelLine: {
+//                 lineStyle: {
+//                     color: '#00FFFF',
+//                 },
+//             },
+//             // hoverAnimation: false,
+//             itemStyle: {
+//                 // borderColor: '#00FFFF'
+//                 // shadowColor: '#00FFFF',
+//                 // shadowBlur: 3
+//             },
+//             data: [
+//                 {
+//                     value: 335, name: '图例1',
+//                 },
+//                 { value: 310, name: '图例2' },
+//                 { value: 234, name: '图例3' },
+//                 { value: 135, name: '图例4' },
+//             ]
+//         },
+//     ]
+// }
+// loadBalance.on('mouseover', function (params) {
+//     loadBalanceOption.graphic.elements[0].style.text = [params.percent + '%']
+//     loadBalance.setOption(loadBalanceOption);
+// });
+// loadBalance.on('mouseout', function (p) {
+//     loadBalanceOption.graphic.elements[0].style.text = ['']
+//     loadBalance.setOption(loadBalanceOption);
+// })
+// loadBalance.setOption(loadBalanceOption);
 
-// CPU使用率
-function setCPU(usage) {
+// // CPU使用率
+// function setCPU(usage) {
     var gaugeCpu = echarts.init(document.getElementById('gauge-cpu'));
     var gaugeCpuOption = {
         series: [
             {
                 type: 'gauge',
-                radius: 185,
+                radius: 90,
                 detail: {
                     formatter: '{value}%',
                     backgroundColor: 'rgba(0,0,0,0.3)',
@@ -131,18 +131,18 @@ function setCPU(usage) {
             }
         ]
     }
-    gaugeCpuOption.series[0].data[0].value = usage;
+    // gaugeCpuOption.series[0].data[0].value = usage;
     gaugeCpu.setOption(gaugeCpuOption);
-}
+// }
 
-// 内存使用率
-function setMemory(usage) {
+// // 内存使用率
+// function setMemory(usage) {
     var gaugeMemory = echarts.init(document.getElementById('gauge-memory'));
     var gaugeMemoryOption = {
         series: [
             {
                 type: 'gauge',
-                radius: 185,
+                radius: 90,
                 detail: {
                     formatter: '{value}%',
                     backgroundColor: 'rgba(0,0,0,0.3)',
@@ -175,19 +175,19 @@ function setMemory(usage) {
             }
         ]
     }
-    gaugeMemoryOption.series[0].data[0].value=usage;
+    // gaugeMemoryOption.series[0].data[0].value = usage;
     gaugeMemory.setOption(gaugeMemoryOption);
 
-}
+// }
 
-// 磁盘使用率
-function setDisk(usage) {
+// // 磁盘使用率
+// function setDisk(usage) {
     var gaugeDisk = echarts.init(document.getElementById('gauge-disk'));
     var gaugeDiskOption = {
         series: [
             {
                 type: 'gauge',
-                radius: 185,
+                radius: 90,
                 detail: {
                     formatter: '{value}%',
                     backgroundColor: 'rgba(0,0,0,0.3)',
@@ -220,9 +220,9 @@ function setDisk(usage) {
             }
         ]
     }
-    gaugeDiskOption.series[0].data[0].value=usage;
+    // gaugeDiskOption.series[0].data[0].value = usage;
     gaugeDisk.setOption(gaugeDiskOption);
-}
+// }
 
 // 可用探测点使用率
 var gaugePoint = echarts.init(document.getElementById('gauge-point'));
@@ -230,12 +230,12 @@ var gaugePointOption = {
     series: [
         {
             type: 'gauge',
-            radius: 185,
+            radius: 90,
             detail: {
                 formatter: '{value}%',
                 backgroundColor: 'rgba(0,0,0,0.3)',
                 color: '#FFFF00',
-                fontSize: 34,
+                fontSize: 17,
                 padding: [10, 5, 0, 5]
             },
             data: [
@@ -264,3 +264,46 @@ var gaugePointOption = {
     ]
 }
 gaugePoint.setOption(gaugePointOption);
+
+// 政务云计算情况
+(function () {
+    var chart = echarts.init(document.getElementById('govChart'));
+    var option = {
+        color: ['#3398DB'],
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+            }
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis: [
+            {
+                type: 'category',
+                data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+                axisTick: {
+                    alignWithLabel: true
+                }
+            }
+        ],
+        yAxis: [
+            {
+                type: 'value'
+            }
+        ],
+        series: [
+            {
+                name: '直接访问',
+                type: 'bar',
+                barWidth: '60%',
+                data: [10, 52, 200, 334, 390, 330, 220]
+            }
+        ]
+    };
+    chart.setOption(option);
+})()
